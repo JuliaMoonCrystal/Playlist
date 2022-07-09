81a709b260c3ea4d798ca7dcca924dd01364355b
Julia Farias da Rocha <jufarias20002@gmail.com>
2022-07-09 12:53:42 -0300

    att

--- index.js
+++ index.js
@@ -2,18 +2,20 @@ require('dotenv').config();
 const express = require('express')
 const connet =require('./database/db')
 const music = require('./model/Music')
+const path = require('path')
 
 
 const app = express()
 const port=process.env.PORT || 3000;
 
 app.set("view engine","ejs")
-app.use(express.static())
+app.use(express.static(path.join(__dirname,'public')))
+app.use(express.urlencoded())
 
 connet();
 
-app.get('/hello', function (req, res) {
-    res.send(`${port}`)
+app.get('/', function (req, res) {
+    res.render("index")
 })
 
 app.listen(3000, () => {
