108880a86e78ea1eae68781a2ab46638d1760cfb
Julia Farias da Rocha <jufarias20002@gmail.com>
2022-07-09 15:35:56 -0300

    style

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const path = require('path')
 const app = express()
 const port=process.env.PORT || 3000;
 
-app.set("view engine","ejs")
-app.use(express.static(path.join(__dirname,'public')))
-app.use(express.urlencoded())
+app.set("view engine", "ejs");
+app.use(express.static(__dirname,+"public"));
+app.use(express.urlencoded());
 
 connet();
 
@@ -19,8 +19,9 @@ app.get('/', async function (req, res) {
     res.render("index",{playlist})
 })
 
-app.get('/admin', function (req, res) {
-    res.render("admin")
+app.get('/admin', async function (req, res) {
+    const playlist = await Music.find();
+    res.render("admin", { playlist, music: null, musicDel: null });
 })
 
 app.post('/create', async function (req, res) {
@@ -29,6 +30,29 @@ app.post('/create', async function (req, res) {
   res.redirect('/')
 
 })
+
+app.get("/by/:id/:action", async (req, res) => {
+    const { id, action } = req.params;
+    music = await Music.findById({ _id: id });
+    const playlist = await Music.find();
+    if (action == "edit") {
+      res.render("admin", { playlist, music, musicDel: null });
+    } else {
+      res.render("admin", { playlist, music: null, musicDel: music });
+    }
+  });
+  
+  app.post("/update/:id", async (req, res) => {
+    const newMusic = req.body;
+    await Music.updateOne({ _id: req.params.id }, newMusic);
+    res.redirect("/admin");
+  });
+  
+  app.get("/delete/:id", async (req, res) => {
+    await Music.deleteOne({ _id: req.params.id });
+    res.redirect("/admin");
+  });
+  
 
 app.listen(3000, () => {
  console.log(`servidor rodando em ${port}`);
